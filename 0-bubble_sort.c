a0adcc2dbd65b6d7b2a371a8c6a730d5f319d970
rayenhdr93 <hedrirayen69@gmail.com>
2022-02-12 02:28:05 +0100

    0-

---
+++ 0-bubble_sort.c
@@ -0,0 +1,26 @@
+#include "sort.h"
+/**
+ * @brief 
+ * 
+ */
+void bubble_sort(int *array, size_t size)
+{
+    int x = 1, j;
+    size_t i;
+
+    while (x > 0)
+    {
+        x = 0;
+        for (i = 0; i < (size - 1); i++)
+            {  
+                if (array[i] > array[i + 1])
+                {
+                    j = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = j;
+                    x += 1;
+                    print_array(array, size);
+                }
+            }
+    }  
+}
