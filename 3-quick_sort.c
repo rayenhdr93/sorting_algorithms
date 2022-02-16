f964c3ce7302e7e61fa28884e235a99ec9dac7fe
rayenhdr93 <hedrirayen69@gmail.com>
2022-02-16 12:48:26 +0100

    quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -3,7 +3,7 @@
  * 
  * 
  */
-int partition(int *array, int fisrt, int last)
+int partition(int *array, int fisrt, int last, size_t size)
 {
 	int pivot = array[last], x = 0;
 	int i = (fisrt - 1), j;
@@ -18,7 +18,7 @@ int partition(int *array, int fisrt, int last)
 			x = array[i];
 			array[i] = array[j];
 			array[j] = x;
-			print_array(array, 10);
+			print_array(array, size);
 			}
 		}
 	}
@@ -27,7 +27,7 @@ int partition(int *array, int fisrt, int last)
 		x = array[i + 1];
 		array[i + 1] = array[last];
 		array[last] = x;
-		print_array(array, 10);
+		print_array(array, size);
 	}
 	return (i + 1);
 }
@@ -36,14 +36,14 @@ int partition(int *array, int fisrt, int last)
  * @array: array of integers
  * @size: size of array
  */
-void quickSort(int *array, int fisrt, int last)
+void quickSort(int *array, int fisrt, int last, size_t size)
 {
 	int pi;
 	if (fisrt < last)
 	{
-		pi = partition(array, fisrt, last);
-		quickSort(array, fisrt, pi - 1);
-		quickSort(array, pi + 1, last);
+		pi = partition(array, fisrt, last, size);
+		quickSort(array, fisrt, pi - 1, size);
+		quickSort(array, pi + 1, last, size);
 	}
 }
 /**
@@ -53,5 +53,7 @@ void quickSort(int *array, int fisrt, int last)
  */
 void quick_sort(int *array, size_t size)
 {
-	quickSort(array, 0, size - 1);
+	if (size < 1)
+		return;
+	quickSort(array, 0, size - 1, size);
 }
